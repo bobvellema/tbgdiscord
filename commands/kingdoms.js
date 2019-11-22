d69f3ef532481a3ab01669599a058605c3624a10
bobvellema <bobvellema02@gmail.com>
2019-11-22 23:45:39 +0100

    Init
    Init

---
+++ commands/kingdoms.js
@@ -0,0 +1,18 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    if (command === `${prefix}kingdoms`) 
+
+    var embedalgemeen = new discord.RichEmbed()
+    .setDescription("kingdoms")
+    .setColor("#46a7f2")
+    .addField("Kingdoms zijn", "Lorum")
+    .setTimestamp()
+    .setFooter("kingdoms", bot.user.displayAvatarURL);
+    
+}
+
+module.exports.help = {
+    name: "kingdoms"
+}
